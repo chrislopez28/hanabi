47a8ddf79adcb0e43d461c8fbc04837533b50224
Chris Lopez <chrislopez28@gmail.com>
2020-02-19 22:35:18 -0800

    Cleaned docstrings

--- hanabi.py
+++ hanabi.py
@@ -3,23 +3,23 @@
 import random
 
 class Card:
-    """Class for a hanabi playing card"""
+    """Class for a Hanabi playing card."""
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
 
     def show(self):
-        """Prints card value and suit"""
+        """Prints card value and suit."""
         print("{} of {}".format(self.value, self.suit))
 
 class Deck:
-    """Class for the playing card deck"""
+    """Class for the playing card deck."""
     def __init__(self):
         self.cards = []
         self.build()
 
     def build(self):
-        "Generates a complete deck of Hanabi cards"
+        """Generates a complete deck of Hanabi cards."""
         for suit in ["Blue", "Green", "Red", "White", "Yellow"]:
             for val in range(1, 6):
                 self.cards.append(Card(suit, val))
@@ -29,22 +29,22 @@ class Deck:
                     self.cards.append(Card(suit, val))
 
     def show(self):
-        "Prints cards in the deck object"
+        """Prints cards in the deck object."""
         for card in self.cards:
             card.show()
 
     def shuffle(self):
-        "Randomizes the order of cards in the deck"
+        """Randomizes the order of cards in the deck."""
         for i in range(len(self.cards) - 1, 0, -1):
             selected = random.randint(0, i)
             self.cards[i], self.cards[selected] = self.cards[selected], self.cards[i]
 
     def draw_card(self):
-        "Pops top card from the deck"
+        """Pops top card from the deck."""
         return self.cards.pop()
 
 class Board:
-    "Class for the game board"
+    """Class for the game board."""
     def __init__(self):
         self.blue = []
         self.green = []
@@ -54,7 +54,7 @@ class Board:
         self.chances = 3
 
     def add(self, card):
-        "Add card to the board"
+        """Add card to the board."""
         if card.suit == "Blue":
             return self.blue
         if card.suit == "Green":
@@ -68,10 +68,11 @@ class Board:
         return None
 
     def check_play(self, card):
-        "Return true if card can be played on the board"
+        """Return true if card can be played on the board."""
         pass
 
     def check_next(self, card):
+        """Check if card is the next card for the suit"""
         pass
 ### Play card to Board
 # if val >= 2 & val <= 5:
@@ -86,18 +87,18 @@ class Board:
 #       self.chances = self.chances - 1
 
 class Player:
-    "Class for a player"
+    """Class for a player."""
     def __init__(self, name):
         self.name = name
         self.hand = []
 
     def draw(self, deck):
-        "Draw a card from the deck into a player's hand"
+        """Draw a card from the deck into a player's hand."""
         self.hand.append(deck.draw_card())
         return self
 
     def show_hand(self):
-        "Print cards in a player's hand"
+        """Print cards in a player's hand."""
         for card in self.hand:
             card.show()
 
